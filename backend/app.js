116c9e08db7c85de2d7fc96d44474ace65442a12
Foucsi <julien.foucartreact@gmail.com>
2024-09-20 12:11:59 +0200

    install dpendencies

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,11 @@
+require("dotenv").config();
+require("./src/models/connection");
+
 var express = require('express');
+
+const errorHandler = require("./src/api/middlewares/errorHandler")
+
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -7,6 +14,10 @@ var indexRouter = require('./src/api/routes');
 var usersRouter = require('./src/api/routes/users');
 
 var app = express();
+const fileUpload = require("express-fileupload");
+app.use(fileUpload());
+const cors = require("cors");
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +27,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+
+//Warning, this generic middlewares should be the last one
+app.use(errorHandler);
 
 module.exports = app;
---
+++ backend/src/models/connection.ts
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const connectionString = process.env.CONNECTION_STRING;
+
+//connection to the database
+const connectDb = async ()=>{
+    try{
+        await mongoose.connect(connectionString, {connectTimeoutMS: 2000});
+        console.log("Successfully connected to the database: boiteAlivres");
+    }catch(error){
+        console.error("Database connection failed. Retrying in 5 seconds...", error);
+        setTimeout(connectDb, 5000);
+    }
+}
+connectDb();
